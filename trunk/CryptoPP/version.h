dac8243d117236528149b2350be4e7b357ed80ad
balookrd <balookrd@d641cd86-4a32-0410-994c-055de1cff029>
2007-06-07 08:24:13 +0000

    - first checkin

--- trunk/CryptoPP/version.h
+++
@@ -1,3 +0,0 @@
-#define __FILEVERSION_STRING        1,0,2,0
-#define __VERSION_STRING            "1.0.2.0"
-#define __VERSION_DWORD             0x01000200
--- trunk/SecureIM/version.h
+++
@@ -1,3 +0,0 @@
-#define __FILEVERSION_STRING        1,0,10,0
-#define __VERSION_STRING            "1.0.10.0"
-#define __VERSION_DWORD             0x01000A00
