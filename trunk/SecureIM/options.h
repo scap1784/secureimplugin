6d6dd258a955e96a494b78920f704ed8b4fd8ecd
balookrd <balookrd@d641cd86-4a32-0410-994c-055de1cff029>
2007-06-07 08:32:58 +0000

    - add SecureIM project

---
+++ trunk/SecureIM/options.h
@@ -0,0 +1,34 @@
+#ifndef __OPTIONS_H__
+#define __OPTIONS_H__
+
+BOOL CALLBACK OptionsDlgProc(HWND,UINT,WPARAM,LPARAM);
+BOOL CALLBACK DlgProcOptionsGeneral(HWND,UINT,WPARAM,LPARAM);
+BOOL CALLBACK DlgProcOptionsProto(HWND,UINT,WPARAM,LPARAM);
+BOOL CALLBACK DlgProcOptionsPGP(HWND,UINT,WPARAM,LPARAM);
+BOOL CALLBACK DlgProcOptionsGPG(HWND,UINT,WPARAM,LPARAM);
+BOOL CALLBACK DlgProcSetPSK(HWND,UINT,WPARAM,LPARAM);
+void ApplyGeneralSettings(HWND);
+void ApplyProtoSettings(HWND);
+void ApplyPGPSettings(HWND);
+void ApplyGPGSettings(HWND);
+void RefreshGeneralDlg(HWND);
+void RefreshProtoDlg(HWND);
+void RefreshPGPDlg(HWND,BOOL);
+void RefreshGPGDlg(HWND,BOOL);
+void ResetGeneralDlg(HWND);
+void ResetProtoDlg(HWND);
+LPARAM getListViewParam(HWND,UINT);
+UINT getListViewStatus(HWND,UINT);
+void setListViewStatus(HWND,UINT,UINT);
+UINT getListViewPSK(HWND,UINT);
+void setListViewPSK(HWND,UINT,UINT);
+int onRegisterOptions(WPARAM,LPARAM);
+int CALLBACK CompareFunc(LPARAM,LPARAM,LPARAM);
+void ListView_Sort(HWND,LPARAM);
+BOOL ShowSelectKeyDlg(HWND,LPSTR);
+LPSTR LoadKeys(LPCSTR,BOOL);
+
+#define getListViewContact(h,i)	(HANDLE)getListViewParam(h,i)
+#define getListViewProto(h,i)	(char*)getListViewParam(h,i)
+
+#endif
---
+++ trunk/SecureIM/resource.h
@@ -0,0 +1,121 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resource.rc
+//
+#define ID_FIRSTICON					1
+#define IDI_GREENKEY                    1
+#define IDI_YELLOWKEY                   2
+#define IDI_REDKEY                      3
+#define IDI_BLUEKEY                     4
+#define IDI_GREYKEY                     5
+#define IDI_COLORKEY                    6
+#define IDI_LOCK                        7
+#define IDI_DISKEY						8
+#define IDI_ENAKEY						9
+#define IDI_TRYKEY						10
+#define IDI_PGPKEY						11
+#define IDI_GPGKEY						12
+#define ID_LASTICON						12
+
+#define ID_DISABLED                     111
+#define ID_ENABLED                      112
+#define ID_ALWAYS                       113
+#define ID_SETPSK                       114
+#define ID_DELPSK                       115
+#define ID_UPDATE_CLIST                 116
+#define ID_UPDATE_PROTO                 117
+#define IDM_CLIST                    	118
+
+#define IDD_OPTIONSTAB                  501
+#define IDD_TAB_GENERAL                 502
+#define IDD_TAB_PROTO                   503
+#define IDD_TAB_PGP                     504
+#define IDD_TAB_GPG                     505
+#define IDD_TAB_MESSAGES                506
+#define IDD_POPUP                       507
+#define IDD_PSK							508
+
+#define IDC_STD_USERLIST                1001
+#define IDC_TIMEOUT                     1002
+#define IDC_BACKKEY                     1003
+#define IDC_TEXTKEY                     1004
+#define IDC_PREV                        1005
+#define IDC_BACKSEC                     1006
+#define IDC_TEXTSEC                     1007
+#define IDC_RESET                       1008
+#define IDC_ALWAYSICON                  1009
+#define IDC_EC                          1010
+#define IDC_DC                          1011
+#define IDC_KS                          1012
+#define IDC_KR                          1013
+#define IDC_SS                          1014
+#define IDC_SR                          1015
+#define IDC_BACKSR                      1016
+#define IDC_BACKKEY6                    1017
+#define IDC_TEXTSR                      1018
+#define IDC_TIMEKEY                     1019
+#define IDC_TIMESEC                     1020
+#define IDC_TIMESR                      1021
+#define IDC_PVALUES                     1022
+#define IDC_ADD                         1023
+#define IDC_DELETE                      1024
+#define IDC_SFT                         1025
+#define IDC_ASI                         1026
+#define IDC_SOM                         1027
+#define IDC_ADV0                        1028
+#define IDC_ADV1                        1029
+#define IDC_ADV2                        1030
+#define IDC_ADV3                        1031
+#define IDC_ADV4                        1032
+#define IDC_ADV5                        1033
+#define IDC_ADV6                        1034
+#define IDC_ADV7                        1035
+#define IDC_ADV8                        1036
+#define IDC_MCD                         1037
+#define IDC_KET                         1038
+#define IDC_OPTIONSTAB                  1039
+#define IDC_PROTO                       1040
+#define IDC_EDIT1                       1041
+#define IDC_EDIT2                       1042
+#define IDC_SCM                         1043
+#define IDC_DGP                         1044
+#define IDC_OKT                         1045
+#define IDC_AIP                         1046
+
+#define IDC_NO_PGP                      1100
+#define IDC_PGP                         1101
+#define IDC_GPG                         1102
+
+#define IDC_PGP_SDK                     1200
+#define IDC_PGP_USERLIST                1201
+#define IDC_SET_KEYRINGS                1202
+#define IDC_KEYRING_STATUS              1203
+#define IDC_PGP_PRIVKEY                 1204
+#define IDC_LOAD_PRIVKEY                1205
+#define IDC_NO_KEYRINGS                 1206
+
+#define IDC_GPGEXECUTABLE_EDIT          1300
+#define IDC_BROWSEEXECUTABLE_BTN        1301
+#define IDC_GPGHOME_EDIT                1302
+#define IDC_SECRETKEY_DDOWN             1303
+#define IDC_SECRETREFRESH_BTN           1304
+#define IDC_GPG_USERLIST                1305
+#define IDC_PUBLICKEY_DDOWN             1306
+#define IDC_PUBLICREFRESH_BTN           1307
+#define IDC_LOGGINGON_CBOX              1308
+#define IDC_GPGLOGFILE_EDIT				1309
+#define IDC_SAVEPASS_CBOX               1310
+
+#define IDC_STATIC                      -1
+#define ID_ENCRYPTION                   40001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        119
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1400
+#define _APS_NEXT_SYMED_VALUE           119
+#endif
+#endif
