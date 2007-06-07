d42887f65e61613e526a0b136bf765e2c781ab3a
balookrd <balookrd@d641cd86-4a32-0410-994c-055de1cff029>
2007-06-07 08:32:58 +0000

    - add SecureIM project

---
+++ SecureIM/popupOptions.h
@@ -0,0 +1,8 @@
+#ifndef __POPUP_OPTIONS__
+#define __POPUP_OPTIONS__
+
+BOOL CALLBACK PopOptionsDlgProc(HWND,UINT,WPARAM,LPARAM);
+void RefreshPopupOptionsDlg(HWND,HWND,HWND,HWND,HWND,HWND);
+int onRegisterPopOptions(WPARAM,LPARAM);
+
+#endif
---
+++ SecureIM/version.h
@@ -0,0 +1,3 @@
+#define __FILEVERSION_STRING        1,0,10,0
+#define __VERSION_STRING            "1.0.10.0"
+#define __VERSION_DWORD             0x01000A00
