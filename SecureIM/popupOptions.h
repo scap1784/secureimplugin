99be5cf161d7bc445f73e939e613a1050ac0daa9
balookrd <balookrd@d641cd86-4a32-0410-994c-055de1cff029>
2007-06-08 06:05:46 +0000

    Tagging the 1.0.2.1 release of the 'CryptoPP' project.

--- SecureIM/popupOptions.h
+++
@@ -1,8 +0,0 @@
-#ifndef __POPUP_OPTIONS__
-#define __POPUP_OPTIONS__
-
-BOOL CALLBACK PopOptionsDlgProc(HWND,UINT,WPARAM,LPARAM);
-void RefreshPopupOptionsDlg(HWND,HWND,HWND,HWND,HWND,HWND);
-int onRegisterPopOptions(WPARAM,LPARAM);
-
-#endif
--- SecureIM/version.h
+++
@@ -1,3 +0,0 @@
-#define __FILEVERSION_STRING        1,0,10,1
-#define __VERSION_STRING            "1.0.10.1"
-#define __VERSION_DWORD             0x01000A01
