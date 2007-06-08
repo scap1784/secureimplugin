7dbd517874134aa0146b4ea219849aaccac2f2ee
balookrd <balookrd@d641cd86-4a32-0410-994c-055de1cff029>
2007-06-08 05:32:57 +0000

    Delete release dir

--- tags/release-1.0.10.1/secureimplugin/CryptoPP/GPGw/passdialog.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PASSDIALOG_H__
-#define __PASSDIALOG_H__
-
-extern char dlgpassphrase[];
-
-BOOL CALLBACK PassphraseDialogProcedure(HWND hdlg, UINT msg, WPARAM wparam, LPARAM lparam);
-
-#endif // __PASSDIALOG_H__
