5202276533b849f46090778838e9a1d889763484
balookrd <balookrd@d641cd86-4a32-0410-994c-055de1cff029>
2007-06-08 06:00:50 +0000

    Delete release dir

--- tags/release-1.0.10.1/trunk/CryptoPP/gettime.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GETTIME_H__
-#define __GETTIME_H__
-
-#include <windows.h>
-
-//int gettime(struct timespec *ts);
-DWORD gettime(void);
-
-#endif
--- tags/release-1.0.10.1/trunk/SecureIM/loadicons.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LOADICONS_H__
-#define __LOADICONS_H__
-
-HINSTANCE LoadIconsPack(const char*);
-void InitIcons();
-
-#endif
