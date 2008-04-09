2b440ea80239e0160e23a9ff8ef05785043611cd
balookrd <balookrd@d641cd86-4a32-0410-994c-055de1cff029>
2008-04-09 07:19:52 +0000

    - fix store subcontact history in metacontacts (only with today meta svn)

--- SecureIM/version.h
+++ SecureIM/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        1,0,10,7
-#define __VERSION_STRING            "1.0.10.7"
-#define __VERSION_DWORD             0x01000A07
+#define __FILEVERSION_STRING        1,0,10,8
+#define __VERSION_STRING            "1.0.10.8"
+#define __VERSION_DWORD             0x01000A08
